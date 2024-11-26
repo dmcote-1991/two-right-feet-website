39fa2ab534bcdeaefe6bdbeeb6a3a765b499e2b7
David Cote <dmc1991@icloud.com>
2024-11-26 13:48:36 -0500

    Add Article model

---
+++ backend/src/models/Article.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+// Article schema
+const articleSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  category: String,
+  content: mongoose.Schema.Types.Mixed // For React components or HTML content
+});
+
+// Create the Article model
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
